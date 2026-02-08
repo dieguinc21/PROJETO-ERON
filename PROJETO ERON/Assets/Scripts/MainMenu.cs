41f0910c7cb98ee6ab69e71154f678a8a6632082
Diego Cabral <145566485+dieguinc21@users.noreply.github.com>
2026-02-08 17:22:01 -0300

    tela de menu programada

---
+++ PROJETO ERON/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+}
